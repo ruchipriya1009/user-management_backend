2f67cde94f12e58bfcb37acb72cae9fdc5d05bb2
--ruchi <ruchipriya1009@gmail.com>
2024-06-22 13:49:46 +0530

    Add backend file

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "bigsecretisveryhardtoguessbysneakypeopleright",
+    EMAIL: "shanie.johns@ethereal.email",
+    PASSWORD : "cXv5a2M8SrWAa3WkBe",
+    ATLAS_URI: "mongodb+srv://ruchipriya1009:2jqO04W92cycJwKf@cluster0.85dubgb.mongodb.net/"
+}
